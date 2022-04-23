af8ce3bed3eeb5ef8042cc60489b3723b3165c0c
Ben Osborne <osborne.ben@gmail.com>
2022-04-23 12:08:10 -0500

    moving tests to correct class

--- src/Pictura.Vita.Utility.Tests/StringExtensionsTests.cs
+++ src/Pictura.Vita.Utility.Tests/StringExtensionsTests.cs
@@ -1,4 +1,5 @@
 ﻿using FluentAssertions;
+using System;
 using Xunit;
 
 namespace Pictura.Vita.Utility.Tests;
@@ -35,5 +36,76 @@ test
 
         // assert
         result.Should().Be(expectedOut);
+    }
+
+    private readonly DateOnly _sampleDate1 = new(2000, 1, 1);
+
+    [Fact]
+    public void ToDateOnlyOrDefault_Valid_Date()
+    {
+        // arrange
+
+        // act
+        var result = "2000-01-01".ToDateOnlyOrDefault();
+
+        // assert
+        result.Should().Be(_sampleDate1);
+    }
+
+    [Theory]
+    [InlineData("")]
+    [InlineData("not a valid date")]
+    public void ToDateOnlyOrDefault_Empty_Or_Invalid_String(string input)
+    {
+        // arrange
+
+        // act
+        var result = input.ToDateOnlyOrDefault();
+
+        // assert
+        result.Should().BeNull();
+    }
+
+    [Theory]
+    [InlineData("")]
+    [InlineData("not a valid date")]
+    public void ToDateOnlyOrDefault_Empty_Or_Invalid_String_Use_Default(string input)
+    {
+        // arrange
+
+        // act
+        var result = input.ToDateOnlyOrDefault(_sampleDate1);
+
+        // assert
+        result.Should().Be(_sampleDate1);
+    }
+
+    [Fact]
+    public void ToDateOnly_Valid_Date()
+    {
+        // arrange
+
+        // act
+        var result = "2000-01-01".ToDateOnly();
+
+        // assert
+        result.Should().Be(_sampleDate1);
+    }
+
+    [Theory]
+    [InlineData(default)]
+    [InlineData("")]
+    [InlineData("not a valid date")]
+    public void ToDateOnly_Empty_Or_Invalid_String(string input)
+    {
+        // arrange
+
+        // act
+        Action result = () => {
+            input.ToDateOnly();
+        };
+
+        // assert
+        result.Should().Throw<Exception>();
     }
 }
