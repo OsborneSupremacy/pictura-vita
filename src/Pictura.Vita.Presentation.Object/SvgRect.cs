c0448c6a6e40c1f7136b1a33ec193a1ce814fba8
Ben Osborne <osborne.ben@gmail.com>
2022-04-26 12:51:47 -0500

    updated SvgRect to use tagbuilder

--- src/Pictura.Vita.Presentation.Object/SvgRect.cs
+++ src/Pictura.Vita.Presentation.Object/SvgRect.cs
@@ -1,10 +1,24 @@
 ﻿
+using System.Text;
+
 namespace Pictura.Vita.Presentation.Object
 {
     public record SvgRect : StructuralElement
     {
-        public override string RenderOpen() =>
-            $@"<rect x=""{X}"" y=""{Y}"" width=""{Width}"" height=""{Height}"" style=""fill:rgb(0,0,255); stroke-width:3;stroke:rgb(0,0,0)"" />";
+        public override string RenderOpen()
+        {
+            StringBuilder s = new();
+
+            var rect = new TagBuilder("rect")
+                .AddAttribute("x", X)
+                .AddAttribute("y", Y)
+                .AddAttribute("width", Width)
+                .AddAttribute("height", Height)
+                .AddAttribute("style", "fill:rgb(0,0,255); stroke-width:3; stroke:rgb(0,0,0)")
+                .MakeSelfClosed();
+
+            return rect.RenderOpen();
+        }
 
         public override string RenderClose() => String.Empty;
     }
