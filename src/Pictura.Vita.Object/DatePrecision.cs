e853735c036d84652101dd9aae5a9e45ccd7499f
Ben Osborne <osborne.ben@gmail.com>
2022-05-14 14:31:28 -0500

    removed enum values for date precision members

--- src/Pictura.Vita.Object/DatePrecision.cs
+++ src/Pictura.Vita.Object/DatePrecision.cs
@@ -2,16 +2,16 @@
 {
     public enum DatePrecision
     {
-        Exact = 0,
-        Week = 1,
-        Month = 2,
-        Year = 3,
-        TwoYears = 4,
-        ThreeYears = 5,
-        FiveYears = 6,
-        Decade = 7,
-        TwentyFiveYears = 7,
-        FiftyYears = 8,
-        Century = 9
+        Exact,
+        Week,
+        Month,
+        Year,
+        TwoYears,
+        ThreeYears,
+        FiveYears,
+        Decade,
+        TwentyFiveYears,
+        FiftyYears,
+        Century
     }
 }
