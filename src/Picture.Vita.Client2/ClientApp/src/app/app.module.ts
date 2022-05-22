1c426bc0dc199f278147118b1badbad31c7f8a91
Ben Osborne <osborne.ben@gmail.com>
2022-05-22 15:00:25 -0500

    added main component

--- src/Picture.Vita.Client2/ClientApp/src/app/app.module.ts
+++ src/Picture.Vita.Client2/ClientApp/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavMenuComponent
+    NavMenuComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
